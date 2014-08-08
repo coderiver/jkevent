f952d05dfea58db18f4d079dad9ae58181c862d5
buschidos <buschidos@buschidos.local>
2014-08-08 10:50:55 +0300

    change animation in popup

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 head.load("js/lib/jquery.js",
 					"js/lib/cycle.js",
 					"js/lib/gallery.js",
-					"js/lib/modernizr.js",
+					// "js/lib/modernizr.js",
 					"js/lib/jquery.easings.min.js",
 					"js/lib/jquery.slimscroll.min.js",
 					"js/lib/jquery.fullPage.min.js",
