982d483e5cd37f9213bf90b0d817ed906d12281b
buschidos <buschidos@buschidos.lan>
2014-08-10 18:52:40 +0300

    almost done

--- js/common.js
+++ js/common.js
@@ -1,25 +1,21 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
 	$('#fullpage').fullpage({
 		verticalCentered: false,
 		easing: 'easeInQuart',
-		// verticalCentered: true,
 		menu: true,
-		// navigation: true,
 		css3: true,
 		scrollingSpeed: 500,
 		scrollOverflow: true,
+		normalScrollElements: '.reviews, .review__text, .offer, .gallery',
+		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+		menu: '.js-nav',
+		// verticalCentered: true,
+		// navigation: true,
 		// touchSensitivity: 15,
 		// paddingTop: '103px',
 		// paddingBottom: '83px',
 		// fixedElements: '.header, .nav, .nav__left, nav__right',
-		normalScrollElements: '.reviews, .review__text, .offer',
-		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
-		menu: '.js-nav',
 
 		afterLoad: function(anchorLink, index){
 			var paginator = $('.pagi'),
@@ -50,46 +46,80 @@ head.ready(function() {
 				header.find('.social').removeClass('is-visible');
 			};
 
-			// if (index == '2' || index == '3' || index == '4' && index++) {
-			// 	header.find('.header__logo').addClass('is-rotate');
-			// }
-
 		},
 
-		// onLeave: function(index, nextIndex, direction){
-		// 	var	activePage = $('.section.active'),
-		// 		prevPage = $('.section.active').prev('.section'),
-		// 		nextPage = $('.section.active').next('.section');
+		onLeave: function(index, nextIndex, direction){
+			var prevPage = $('.section.active').prev('.section'),
+				nextPage = $('.section.active').next('.section');
 
-		// 	if (direction =='down') {
-		// 		// activePage.addClass('is-visible');
-		// 		prevPage.addClass('is-hidden')
-		// 	}
-		// 	else if (direction =='up') {
-		// 		activePage.removeClass('is-hidden');
-		// 		// nextPage.removeClass('is-visible');
-		// 		// prevPage.addClass('is-hidden');
-		// 	};
-
-		// }
+			if (direction =='down') {
+				prevPage.addClass('is-slidedown');
+				setTimeout(function() {
+					$('.section').removeClass('is-slidedown');
+				}, 1000)
+			}
+			else if (direction =='up') {
+				nextPage.addClass('is-slideup');
+				setTimeout(function() {
+					$('.section').removeClass('is-slideup');
+				}, 1000)
+			};
+		},
 	});
 
-	$('.offer__container').hover(function() {
-		var slider = $(this).find('.offer__backside-wrap');
-
-		$(this).find('.offer__more').click(function(event) {
-			slider.css('left', '-100%');
-		});
-
-		$(this).find('.offer__back').click(function(event) {
-			slider.css('left', '0');
-		});
+	//flip offer on click
+	$('.offer').click(function(event) {
+		$(this).addClass('is-flipped');
+		showAboutOffer();
+	});
+	$(document).click(function(event) {
+		if ($(event.target).closest('.offer').length == 0) {
+			$('.offer').removeClass('is-flipped');
+		};
+	});
+	//flip offer on hover
+	$('.offer').hover(function() {
+		$(this).addClass('is-flipped');
+		showAboutOffer();
 	}, function() {
-		var slider = $(this).find('.offer__backside-wrap');
-		setTimeout(function() {
-			slider.css('left', '0');
-		}, 400);
+		$(this).removeClass('is-flipped');
 	});
+	//navigation inside offer block
+	function showAboutOffer() {
+		var activeOffer = $('.offer.is-flipped'),
+			slider = activeOffer.find('.offer__backside-wrap'),
+			linkToAbout = activeOffer.find('.offer__content').find('.offer__more'),
+			linkBack = activeOffer.find('.offer__back'),
+			allAbout = activeOffer.find('.offer-about1, .offer-about2, .offer-about3');
+
+		allAbout.hide();
+
+		linkToAbout.click(function(event) {
+			var targetAbout = $(this).attr('href');
+
+			slider.css('left', '-100%');
+			$('.'+targetAbout).show();
+			return false;
+		});
+
+		linkBack.click(function(event) {
+			slider.css('left', '0');
+			setTimeout(function() {
+				allAbout.hide();
+			}, 400);
+			return false;
+		});
+
+		activeOffer.hover(function() {
+			/* Stuff to do when the mouse enters the element */
+		}, function() {
+			setTimeout(function() {
+				slider.css('left', '0');
+				allAbout.hide();
+			}, 400);
+		});
+	};
+
 
 	//show popup
 	$('.btn.btn_offer').click(function(event) {
@@ -111,11 +141,3 @@ head.ready(function() {
 	});
 
 });
-
-
-
-
-
-
-
-
